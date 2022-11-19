c860a64fbabd053e80c8e632096d8c8baef97e07
Silja Sandersen <silja.sandersen@hotmail.com>
2022-11-19 14:27:07 +0100

    Updated build.gradle

--- app/src/main/java/UX/thewheeloffortune/MainActivity.kt
+++ app/src/main/java/UX/thewheeloffortune/MainActivity.kt
@@ -6,8 +6,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     MainScreen()
                 }
--- app/src/main/java/UX/thewheeloffortune/ui/CategoryScreen.kt
+++ app/src/main/java/UX/thewheeloffortune/ui/CategoryScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material.RadioButton
 import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
