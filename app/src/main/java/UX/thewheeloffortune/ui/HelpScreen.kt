f7a209f9a1689ece35077256f9e4536a5b10ec0f
Silja Sandersen <silja.sandersen@hotmail.com>
2022-11-26 10:44:46 +0100

    Added rules to help screen

--- app/src/main/java/UX/thewheeloffortune/ui/HelpScreen.kt
+++ app/src/main/java/UX/thewheeloffortune/ui/HelpScreen.kt
@@ -2,10 +2,7 @@ package UX.thewheeloffortune.ui
 
 import UX.thewheeloffortune.R
 import UX.thewheeloffortune.ui.theme.TheWheelOfFortuneTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +14,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 /**
- * Contains rules of the game
+ * Contains rules of the game (source: assignment description)
  */
 @Composable
 fun HelpScreen(
@@ -27,7 +24,8 @@ fun HelpScreen(
         modifier = modifier
             .fillMaxHeight()
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         item {
             Box(
@@ -40,10 +38,72 @@ fun HelpScreen(
                 )
             }
         }
-
-        // TODO: ADD RULES!
         item {
-
+            Text(text = "1. When the game starts, the player has to choose a category.")
+        }
+        item {
+            Text(
+                text = "2. When a category has been chosen, a word within this " +
+                        "category is randomly chosen for the player to guess."
+            )
+        }
+        item {
+            Text(text = "3. The word the user needs to guess is displayed with the letters hidden.")
+        }
+        item {
+            Text(
+                text = "4. Before guessing a letter, the player needs to spin the wheel by " +
+                        "tapping a button."
+            )
+        }
+        item {
+            Text(text = "5. After spinning the wheel a number of points or \"bankrupt\" is displayed.")
+        }
+        item {
+            Text(
+                text = "6. If \"bankrupt\" is displayed, the player loses all their points. The " +
+                        "player then needs to spin the wheel again."
+            )
+        }
+        item {
+            Text(
+                text = "7. If a value is being shown, the player needs to choose a letter not " +
+                        "yet guessed."
+            )
+        }
+        item {
+            Text(
+                text = "8. If the guessed letter is present in the word, the user's score is " +
+                        "incremented by the value shown times the number of occurrences of the guessed letter. "
+            )
+        }
+        item {
+            Text(
+                text = "9. If the guessed letter is present in the word, the occurrences of the letter" +
+                        "are shown in the hidden word on the screen."
+            )
+        }
+        item {
+            Text(text = "10. If the guessed letter is not present, the player loses a life.")
+        }
+        item {
+            Text(text = "11 The game continues until the game is either won, lost or exited.")
+        }
+        item {
+            Text(
+                text = "12. Winning condition: When all the letters have been guessed correctly, " +
+                        "and the player still has at least one life left."
+            )
+        }
+        item {
+            Text(
+                text = "13. Losing condition: When the player has no lived left and the word has" +
+                        "not been guessed."
+            )
+        }
+        item {
+            Text(text = "14. A player starts with 5 lives")
+            Spacer(modifier = modifier.height(50.dp))
         }
     }
 }
